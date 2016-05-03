c1728ece1f979fd1d50c43c4eb739291cb62bd8e
Eduardo Romano <edo.romanoo@gmail.com>
2016-05-03 15:56:06 -0500

    Add Update action for the user controller and specs

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
-  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
+  before(:each) { request.headers['Accept'] = "application/vnd.geo_analytics.v1" }
 
   describe "GET #show" do
     before(:each) do
@@ -47,7 +47,43 @@ describe Api::V1::UsersController do
         user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
+    end
 
+    describe "PUT/PATCH #update" do
+
+      context "when is successfully updated" do
+        before(:each) do
+          @user = FactoryGirl.create :user
+          patch :update, { id: @user.id,
+                           user: { email: "newmail@example.com" } }, format: :json
+        end
+
+        it "renders the json representation for the updated user" do
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response[:email]).to eql "newmail@example.com"
+        end
+
+        it { should respond_with 200 }
+      end
+
+      context "when is not created" do
+        before(:each) do
+          @user = FactoryGirl.create :user
+          patch :update, { id: @user.id,
+                           user: { email: "bademail.com" } }, format: :json
+        end
+
+        it "renders an errors json" do
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response).to have_key(:errors)
+        end
+
+        it "renders the json errors on whye the user could not be created" do
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response[:errors][:email]).to include "is invalid"
+        end
+
+      end
     end
   end
 end
